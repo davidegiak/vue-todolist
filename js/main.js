c82f2d20401539d6d9442e6f06a9a8df9eec281e
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-14 16:19:32 +0200

    ....

--- js/main.js
+++ js/main.js
@@ -4,15 +4,22 @@ const { createApp } = Vue
     data() {
       return {
         coseDaFare: [
-            "fare la spesa",
+            "fare la spesa ",
             "portare fuori il cane",
             "dare da mangiare ai pesci",
             "falciare il prato",
             "prendere una birra con ciccio",
-            ""
-        ]
+        ],
+        userCoseDaFare: ""
       }
     },
     methods: {
+      stampa() {
+        this.coseDaFare.push(this.userCoseDaFare)
+      },
+      svuota() {
+        this.coseDaFare.classlist.add("remove");
+        
+      }
     }
   }).mount('#app')
